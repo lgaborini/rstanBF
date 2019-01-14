a6cd57ca88840ca5e11c64615bc4100e9e9e5897
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2019-01-14 11:49:38 +0100

    Hidden internal MLE function

--- R/sirt_dirichlet.R
+++ R/sirt_dirichlet.R
@@ -14,7 +14,6 @@ digamma1 <- function(x,h = .001){
 #' Maximum Likelihood Estimation of the Dirichlet Distribution
 #'
 #' Maximum likelihood estimation of the parameters of the Dirichlet distribution
-#' @export
 #' @importFrom sirt dirichlet.mle
 dirichlet.mle <- function( x,  weights=NULL, eps=10^(-5),convcrit=.00001, maxit=1000,
                            oldfac=.3, progress=FALSE){

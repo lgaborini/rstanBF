7fb5fc6c3476bf75dd0e309229a44fbfd9ddd551
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2019-01-16 11:33:29 +0100

    Add sampling to DirDir vignette

--- R/stan_BF_data.R
+++ R/stan_BF_data.R
@@ -2,11 +2,12 @@
 #'
 #' @param list_pop the output of a `rsamplestudy`-generated population
 #' @param list_samples the output of [rsamplestudy::make_dataset_splits()]
+#' @param col_source the name of the source column (default: 'source')
 #' @return a list containing `mtx`, `idx.ref`, `idx.quest`
 #' @export
-stanBF_prepare_rsamplestudy_data <- function(list_pop, list_samples) {
+stanBF_prepare_rsamplestudy_data <- function(list_pop, list_samples, col_source = 'source') {
 
-   mtx_samples <- list_pop$df_pop %>% select(-source) %>% as.matrix
+   mtx_samples <- select(list_pop$df_pop, -col_source) %>% as.matrix
 
    list_data <- list(mtx = mtx_samples, idx.ref = list_samples$idx_reference, idx.quest = list_samples$idx_questioned)
    list_data
